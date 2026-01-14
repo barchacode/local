be363c6ea7f9c7218fbfc8cfcf5733322ae21a9c
barchacode <barcha007786110@gmail.com>
2026-01-15 03:27:44 +0500

    add sql query

---
+++ data/sql wor.sql
@@ -0,0 +1,17 @@
+CREATE TABLE sam (
+  area_sqft   INTEGER,
+  bedrooms    INTEGER,
+  bathrooms   INTEGER,
+  stories     INTEGER,
+  parking     INTEGER,
+  age_years   INTEGER,
+  city        TEXT,
+  near_school TEXT,     -- Yes/No
+  price       INTEGER
+);
+select * from sam;
+
+select near_school,count(*) from sam group by near_school; 
+
+
+SELECT * FROM sam LIMIT 5;
