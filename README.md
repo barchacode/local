7f7eb01062fe3f980991c1314aeca6c98b5d225e
barchacode <barcha007786110@gmail.com>
2026-01-16 21:37:02 +0500

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather Data ETL Pipeline
+
+This project demonstrates a basic ETL pipeline:
+- Extracts weather data from Open-Meteo API
+- Transforms hourly data into structured format
+- Loads cleaned data into PostgreSQL
+
+Tech Stack:
+- Python
+- Pandas
+- PostgreSQL
+- Git
--- python/api_to_db.py
+++ python/api_to_db.py
@@ -1,26 +1,28 @@
 import pandas as pd
 import requests
 from pathlib import Path
+def run_pipeline():
+ base_dir = Path(__file__).resolve().parent.parent
+ url="https://api.open-meteo.com/v1/forecast?latitude=33.6844&longitude=73.0479&hourly=temperature_2m"
+ response = requests.get(url)
+ data = response.json()
 
-base_dir = Path(__file__).resolve().parent.parent
-url="https://api.open-meteo.com/v1/forecast?latitude=33.6844&longitude=73.0479&hourly=temperature_2m"
-response = requests.get(url)
-data = response.json()
+ df = df = pd.DataFrame(data["hourly"])
 
-df = df = pd.DataFrame(data["hourly"])
+ raw_path = base_dir / "data" / "weather_raw.csv"
+ df.to_csv(raw_path, index=False)
 
-raw_path = base_dir / "data" / "weather_raw.csv"
-df.to_csv(raw_path, index=False)
+ print(type(df["time"].iloc[0]))
+ print(df.head())
+ df["time"] = pd.to_datetime(df["time"])
+ df["date"] = df["time"].dt.date
+ df["hour"] = df["time"].dt.hour
 
-print(type(df["time"].iloc[0]))
-print(df.head())
-df["time"] = pd.to_datetime(df["time"])
-df["date"] = df["time"].dt.date
-df["hour"] = df["time"].dt.hour
+ df = df[["time", "date", "hour", "temperature_2m"]]
+ df = df.rename(columns={"temperature_2m": "temperature"})
 
-df = df[["time", "date", "hour", "temperature_2m"]]
-df = df.rename(columns={"temperature_2m": "temperature"})
-
-clean_path = base_dir / "data" / "weather_cleaned.csv"
-df.to_csv(clean_path, index=False)
-
+ clean_path = base_dir / "data" / "weather_cleaned.csv"
+ df.to_csv(clean_path, index=False)
+if __name__ == "__main__":
+    run_pipeline()
+ 
